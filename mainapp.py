63d195e9ce85484151f2f36e9c5acfbcad24c04d
baron070981 <baron070981@gmail.com>
2020-10-02 17:22:03 +0300

    -----

--- mainapp.py
+++ mainapp.py
@@ -46,6 +46,9 @@ class DataListScreen(Screen):
         print('NEW')
 
 
+class MonthDataListScreen(Screen):
+    pass
+
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
                                  RecycleBoxLayout):
     ''' Adds selection and focus behaviour to the view. '''
@@ -75,23 +78,17 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         self.selected = is_selected
         if is_selected:
             print("selection changed to {0}".format(rv.data[index]))
-            print(rv.data[index].get('text').split('/'))
-            vals = rv.data[index].get('text').split('/')
-            data = dp.viewmonth(vals[0].strip(),int(vals[1].strip()))
-            pprint(data)
-            rv.data = data
+            
         else:
             print("selection removed for {0}".format(rv.data[index]))
-
-
 
 
 class PeriodsView(RecycleView):
     def __init__(self,**kwargs):
         super(PeriodsView,self).__init__(**kwargs)
         
-        allperiods = dp.periods(alldata)
-        data = dp.viewmonthsyear()
+        #allperiods = dp.periods(alldata)
+        data = dp.viewmonthsyear(alldata)
         if data == None or len(data) == 0:
             self.data = []
         else:
@@ -99,7 +96,41 @@ class PeriodsView(RecycleView):
     
     
     def into_dataview(self):
-        pass
+        print("Data view...")
+
+
+class SelectableLabel2(RecycleDataViewBehavior, Label):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableLabel2, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableLabel2, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
+
+
+class MonthView(RecycleView):
+    def __init__(self,**kwargs):
+        super(MonthView,self).__init__(**kwargs)
+        self.data = [ { 'text':'month view' } ]
 
 
 
